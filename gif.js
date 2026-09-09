f123d3747cb6ce345dacc86454aee587d5669e47
abbylukwa <ebenezerlukwa@gmail.com>
2026-09-09 23:13:59 +0000

    Switch GIF search from hardgif to GIPHY API

--- gif.js
+++ gif.js
@@ -1,36 +1,16 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
 
 async function search(query) {
-    const url = `https://hardgif.com/search/${encodeURIComponent(query)}`;
-    const headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
-    };
+    // Use GIPHY public beta key (works without signup)
+    const giphyUrl = `https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=${encodeURIComponent(query)}&limit=5`;
     try {
-        const { data } = await axios.get(url, { headers, timeout: 10000 });
-        const $ = cheerio.load(data);
-        const gifUrls = [];
-        $('img[src*=".gif"]').each((i, el) => {
-            let src = $(el).attr('src');
-            if (src) {
-                if (src.startsWith('//')) src = 'https:' + src;
-                if (src.startsWith('/')) src = 'https://hardgif.com' + src;
-                gifUrls.push(src);
-            }
-        });
-        $('a[href*=".gif"]').each((i, el) => {
-            let href = $(el).attr('href');
-            if (href) {
-                if (href.startsWith('//')) href = 'https:' + href;
-                if (href.startsWith('/')) href = 'https://hardgif.com' + href;
-                gifUrls.push(href);
-            }
-        });
-        return [...new Set(gifUrls)];
+        const response = await axios.get(giphyUrl);
+        const gifs = response.data.data.map(g => g.images.original.url);
+        if (gifs.length > 0) return gifs;
     } catch (e) {
-        console.error('GIF search error:', e.message);
-        return [];
+        console.error('GIPHY error:', e.message);
     }
+    return [];
 }
 
 module.exports = { search };
